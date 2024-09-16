ce2f71c80b7f2fd83bf4dfbc213f8610d11d8035
abhisheksah255 <abhisheksah255@gmail.com>
2024-09-16 16:59:59 +0530

    Object creation ways

---
+++ SpringBootPractice/src/main/java/com/abhishek/app/Laptop.java
@@ -0,0 +1,12 @@
+package com.abhishek.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	
+
+	public void typing() {
+		System.out.println("I am typing on Laptop");
+	}
+}
--- SpringBootPractice/src/main/java/com/abhishek/app/SpringBootPracticeApplication.java
+++ SpringBootPractice/src/main/java/com/abhishek/app/SpringBootPracticeApplication.java
@@ -2,12 +2,22 @@ package com.abhishek.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringBootPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPracticeApplication.class, args);
+//Here we are creating the object and it will manage by us
+		Laptop obj=new Laptop();
+		obj.typing();
+		
+//Here object is created by Spring IOC and it will manage by them 
+	ApplicationContext context=	SpringApplication.run(SpringBootPracticeApplication.class, args);
+	Laptop obj1= context.getBean(Laptop.class);
+	obj1.typing();
+	
 	}
 
 }
